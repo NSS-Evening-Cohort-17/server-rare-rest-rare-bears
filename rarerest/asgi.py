492ce3c56558716a2d47113437ec72eee117b200
Chaneice Jackson <c.jackson0126@gmail.com>
2022-06-27 20:51:21 -0500

    set up

--- rarerest/asgi.py
+++ rarerest/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for levelup project.
+ASGI config for rarerest project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'levelup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rarerest.settings')
 
 application = get_asgi_application()
