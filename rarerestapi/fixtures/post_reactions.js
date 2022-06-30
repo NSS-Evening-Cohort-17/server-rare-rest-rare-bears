b0e1b96f5b7265214081da67b89a316f72432fb2
Chaneice Jackson <c.jackson0126@gmail.com>
2022-06-30 01:37:55 -0500

    fixtures step one

---
+++ rarerestapi/fixtures/post_reactions.js
@@ -0,0 +1,26 @@
+[
+    {
+        "model": "rarerestapi.post_reactions",
+        "pk": 1,
+        "fields": {
+            "reaction_id": 1,
+            "post_id": 1
+        }
+    },
+    {
+        "model": "rarerestapi.post_reactions",
+        "pk": 1,
+        "fields": {
+            "reaction_id": 1,
+            "post_id": 2
+        }
+    },
+    {
+        "model": "rarerestapi.post_reactions",
+        "pk": 1,
+        "fields": {
+            "reaction_id": 1,
+            "post_id": 3
+        }
+    }
+]
