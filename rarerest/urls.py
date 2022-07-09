1a5089d248d3b4a2f632a7243294358cadfa78f0
Jake Chestnut <jacobschestnut@gmail.com>
2022-07-09 13:41:54 -0500

    got login/register to function

--- rarerest/urls.py
+++ rarerest/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
+from rarerestapi.views import login_user, register_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
-    # path('register', register_user),
-    # path('login', login_user),
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
     path('', include(router.urls))
 ]
