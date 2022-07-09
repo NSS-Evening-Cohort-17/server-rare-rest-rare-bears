200e6acb4f147c66b0d2bb0fd33b249607df5cc6
Jake Chestnut <jacobschestnut@gmail.com>
2022-07-08 20:48:06 -0500

    loaded posts

--- rarerestapi/models/post.py
+++ rarerestapi/models/post.py
@@ -1,11 +1,12 @@
 from django.db import models
+from platformdirs import user_log_dir
 from rarerestapi.models.category import Category
 from rarerestapi.models.rare_user import RareUser
 
 class Post(models.Model):
     
-    rare_user = models.ForeignKey(RareUser, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(RareUser, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
     publication_date = models.DateField(auto_now=False, auto_now_add=False)
     image_url = models.CharField(max_length=1000)
