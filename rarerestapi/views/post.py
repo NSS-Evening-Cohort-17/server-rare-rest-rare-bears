b3400974a86b671bdf0fc0b56974e65004b2fffb
Jake Chestnut <jacobschestnut@gmail.com>
2022-07-11 20:40:11 -0500

    got Post REST functionality working

---
+++ rarerestapi/views/post.py
@@ -0,0 +1,83 @@
+"""View module for handling requests about posts"""
+from unicodedata import category
+from django.http import HttpResponseServerError
+from django.core.exceptions import ValidationError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rarerestapi.models import Post, RareUser
+
+
+class PostView(ViewSet):
+    """Level up game types view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single post
+
+        Returns:
+            Response -- JSON serialized post
+        """
+        try:
+            post = Post.objects.get(pk=pk)
+            serializer = PostSerializer(post)
+            return Response(serializer.data)
+        except Post.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    
+
+    def list(self, request):
+        """Handle GET requests to get all posts
+
+        Returns:
+            Response -- JSON serialized list of posts
+        """
+        
+        posts = Post.objects.all()
+        category = request.query_params.get('category', None)
+        if category is not None:
+            posts = posts.filter(category_id=category)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized post instance
+        """
+        user = RareUser.objects.get(user=request.auth.user)
+        serializer = CreatePostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk):
+        """Handle PUT requests for a post
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        post = Post.objects.get(pk=pk)
+        serializer = CreatePostSerializer(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+        
+    
+class PostSerializer(serializers.ModelSerializer):
+    """JSON serializer for posts
+    """
+    class Meta:
+        model = Post
+        fields = ['id', 'user', 'category', 'title', 'publication_date', 'image_url', 'content', 'approved']
+        depth = 1
+        
+class CreatePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'category', 'title', 'publication_date', 'image_url', 'content', 'approved']
