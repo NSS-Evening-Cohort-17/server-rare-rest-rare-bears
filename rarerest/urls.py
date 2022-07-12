1496b0e7f065f6831ac6f8db38354efa5c8c91e3
Jake Chestnut <jacobschestnut@gmail.com>
2022-07-11 20:40:54 -0500

    got Post REST functinality working on the backend

--- rarerest/urls.py
+++ rarerest/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from rarerestapi.views import login_user, register_user
+from rarerestapi.views import login_user, register_user, PostView
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'posts', PostView, 'post')
 
 urlpatterns = [
     path('register', register_user),
