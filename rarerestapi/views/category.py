de8f6e367dad85c8a4eb97e25d7fb941f9fa6710
michellecaner <michellecaner@gmail.com>
2022-07-12 21:34:26 -0500

    set up create category request

--- rarerestapi/views/category.py
+++ rarerestapi/views/category.py
@@ -1,5 +1,6 @@
 """View module for handling requests about categories"""
 from django.http import HttpResponseServerError
+from django.core.exceptions import ValidationError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
@@ -20,7 +21,7 @@ class CategoryView(ViewSet):
             return Response(serializer.data)
         except Category.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-        
+
     def list(self, request):
         """Handle GET requests to get all categories
 
@@ -31,9 +32,25 @@ class CategoryView(ViewSet):
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        """Handle POST requests for new category
+
+        Returns
+            Response -- JSON serialized category instance
+        """
+        serializer = CreateCategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 class CategorySerializer(serializers.ModelSerializer):
     """JSON serializer for categories"""
     class Meta:
         model = Category
         fields = ('id', 'label')
         depth = 1
+
+class CreateCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'label']
